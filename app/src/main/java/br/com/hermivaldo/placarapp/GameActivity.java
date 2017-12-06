d30d51d5310e8800e380b39f7b3ee0125146b317
hermivaldo <hermivaldo.braga@gmail.com>
2017-12-06 20:07:42 -0200

    Finalização de botões com evento em tela

--- app/src/main/java/br/com/hermivaldo/placarapp/GameActivity.java
+++ app/src/main/java/br/com/hermivaldo/placarapp/GameActivity.java
@@ -3,6 +3,7 @@ package br.com.hermivaldo.placarapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,8 +21,6 @@ public class GameActivity extends AppCompatActivity {
         time1 = (TextView) findViewById(R.id.timeCasa);
         time2 = (TextView) findViewById(R.id.timeVisitante);
 
-
-        
         Intent intent = getIntent();
         ArrayList<String> times = intent.getStringArrayListExtra(MainActivity.PARAMENTO_TIMES);
         if (times != null) {
@@ -29,5 +28,30 @@ public class GameActivity extends AppCompatActivity {
             time2.setText(times.get(1));
         }
 
+    }
+
+    public void marcarGol(View view){
+
+        Integer idBtnClicado = view.getId();
+        TextView txtPla;
+        int valorVP;
+        switch (idBtnClicado){
+            case (R.id.btGolTime1):
+                findViewById(R.id.timeCasaPlacar);
+                txtPla = (TextView) findViewById(R.id.timeCasaPlacar);
+                valorVP = Integer.parseInt(txtPla.getText().toString());
+                valorVP++;
+
+                txtPla.setText(String.valueOf(valorVP));
+                //Toast.makeText(this, "BTn1", Toast.LENGTH_SHORT).show();
+            break;
+            case (R.id.btGolTime2):
+                txtPla = (TextView) findViewById(R.id.timeVisitantePlacar);
+                valorVP = Integer.parseInt(txtPla.getText().toString());
+                valorVP++;
+                txtPla.setText(String.valueOf(valorVP));
+                //Toast.makeText(this, "BTn2", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
